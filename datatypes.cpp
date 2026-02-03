38d024f913de5ea2f33e24e87864d973a610f50f
Pindi Rupesh <pindirupesh@gmail.com>
2026-02-03 14:21:05 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age = 18;
+    float marks = 85.5;
+    char grade = 'A';
+
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+    return 0;
+}
